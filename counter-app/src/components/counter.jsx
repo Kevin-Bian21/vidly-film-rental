c1072b0b52dd96f34db18aa6f8ae7d8cead48962
BianWenKai <1467295546@qq.com>
2022-01-21 20:16:15 +0800

    绑定事件句柄：1.使用constructor的bind；2.使用箭头函数

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -6,12 +6,23 @@ class Counter extends Component {
         imageUrl:'Https://picsum.photos/10',
         tags : ['a','b','c']
     };
+
+    //将handlerIncrement中的this调用对象绑定到当前类对象
+    // constructor() {
+    //     super();
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    // }
+
+    //使用箭头函数可以继承this的关系，就可以不用constructor来bind
+    handleIncrement = () => {
+        console.log(this);
+    }
     render() {
         return (
             <div>
                 <img src={this.state.imageUrl} alt='图片'/>
                 <span style={{fontSize:15}} className={this.getBadgeClasses()}>{this.state.count}</span>
-                <button className='btn btn-secondary btn-sm'>增加</button>
+                <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>增加</button>
                 <ul>
                     {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
                 </ul>
