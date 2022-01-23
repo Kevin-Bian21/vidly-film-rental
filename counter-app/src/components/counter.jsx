5b62d257352248d9d6783316949734995ed425d2
BianWenKai <1467295546@qq.com>
2022-01-23 23:40:45 +0800

    删除counter.jsx中的state，让该counter组件称为受控组件，保证数据的唯一来源

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   // props是我们给组件的数据，是只读属性；state是组件本地的私有的数据容器，其他组件不能访问该state。
-  state = {
-    count: this.props.counter.value,
-    imageUrl: "Https://picsum.photos/10",
-    tags: ["a", "b", "c"],
-  };
+  // state = {
+  //   count: this.props.counter.value,
+  //   imageUrl: "Https://picsum.photos/10",
+  //   tags: ["a", "b", "c"],
+  // };
 
   //将handlerIncrement中的this调用对象绑定到当前类对象
   // constructor() {
@@ -15,38 +15,26 @@ class Counter extends Component {
   // }
 
   //使用箭头函数可以继承this的关系，就可以不用constructor来bind
-  handleIncrement = (productId) => {
-    console.log("产品ID： "+ productId);
-    this.setState({ count: this.state.count + 1 });
-  };
+
   render() {
-    console.log(this.props);
     return (
       <div>
-        <img src={this.state.imageUrl} alt="图片" />
         <span style={{ fontSize: 15 }} className={this.getBadgeClasses()}>
-          {this.state.count}
+          {this.props.counter.value}
         </span>
         <button
-          onClick={() => this.handleIncrement(this.state.count)}
-          className="btn btn-secondary btn-sm">
-          增加
-        </button>
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm">增加</button>
         <button
           onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger btn-sm m-2">删除</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 bg-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 }
--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -14,14 +14,31 @@ class Counters extends Component {
         const counters = this.state.counters.filter( c => c.id !== counterId);
         this.setState({ counters : counters });
     }
+    handleIncrement = (counter) => {
+        const counters = [...this.state.counters];
+        let index = counters.indexOf(counter);
+        counters[index] = {...counter};
+        counters[index].value ++;
+        this.setState({ counters });
+    };
+    handleReset = () => {
+        //使用map将state中的每个counter对象进行映射，并重置它的value值为0，这样旧的到了一个新的数组
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        });
+        this.setState({ counters });
+    };
 
     render() {
         return (
             <div>
+                <button onClick={this.handleReset} className="btn btn-primary btn m-2">重置</button>
                 { this.state.counters.map(counter => (
                     <Counter
                         key={counter.id}
                         onDelete={this.handleDelete}
+                        onIncrement={this.handleIncrement}
                         counter={counter}
                     />
                 ))}
