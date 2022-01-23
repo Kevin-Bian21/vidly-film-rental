25a58784167d294a3e1192f6a1c1fed793936a5c
BianWenKai <1467295546@qq.com>
2022-01-23 22:53:13 +0800

    发起并处理事件 onDelete(this.props.counter.id)

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  // props是我们给组件的数据，是只读属性；state是组件本地的私有的数据容器，其他组件不能访问该state。
   state = {
-    count: 1,
+    count: this.props.counter.value,
     imageUrl: "Https://picsum.photos/10",
     tags: ["a", "b", "c"],
   };
@@ -19,6 +20,7 @@ class Counter extends Component {
     this.setState({ count: this.state.count + 1 });
   };
   render() {
+    console.log(this.props);
     return (
       <div>
         <img src={this.state.imageUrl} alt="图片" />
@@ -27,10 +29,12 @@ class Counter extends Component {
         </span>
         <button
           onClick={() => this.handleIncrement(this.state.count)}
-          className="btn btn-secondary btn-sm"
-        >
+          className="btn btn-secondary btn-sm">
           增加
         </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2">删除</button>
         <ul>
           {this.state.tags.map((tag) => (
             <li key={tag}>{tag}</li>
