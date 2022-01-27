12db52c295f1e569d51875cac31cc41c0903b8b1
BianWenKai <1467295546@qq.com>
2022-01-27 20:40:27 +0800

    添加 like 组件

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
+import Like from './common/Like';
 
 class Movies extends Component {
     state = {
         movies : getMovies()
-    }
+    };
 
     handleDelete = (movie) => {
         console.log(movie);
         const movies = this.state.movies.filter(m => m._id !== movie._id);  //将传入的movie的id和movies中的id不相同的movie重新选出赋值给state中的movies
         this.setState({ movies : movies})
-    }
+    };
+
+    handleLike = (movie) => {
+        const movies = [...this.state.movies];
+        const index = movies.indexOf(movie);
+        movies[index] = {...movies[index]};
+        movies[index].liked = !movies[index].liked;
+        this.setState({ movies })
+    };
 
     render() {
 
@@ -30,6 +39,7 @@ class Movies extends Component {
                             <th>Stock</th>
                             <th>Rate</th>
                             <th></th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -39,6 +49,9 @@ class Movies extends Component {
                                 <td>{movie.genre.name}</td>
                                 <td>{movie.numberInStock}</td>
                                 <td>{movie.dailyRentalRate}</td>
+                                <td>
+                                    <Like liked = {movie.liked} onClick={() => this.handleLike(movie)} />
+                                </td>
                                 <td>
                                     <button onClick={() => this.handleDelete(movie)} className='btn btn-danger btn-sm'>删除</button>
                                 </td>
