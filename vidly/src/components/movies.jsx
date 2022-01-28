32284e9d1fdc987aca6d555b48e100c6c66c1651
BianWenKai <1467295546@qq.com>
2022-01-28 13:12:32 +0800

    添加分组组件，使用defaultProps使组件解耦合

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -1,6 +1,9 @@
+import { get } from 'lodash';
 import React, { Component } from 'react';
+import { getGenres } from '../services/fakeGenreService';
 import { getMovies } from '../services/fakeMovieService';
 import Like from './common/Like';
+import ListGroup from './common/ListGroup';
 import Pagination from './common/Pagination';
 import  { paginate }  from './utils/paginate';
 
@@ -8,7 +11,8 @@ class Movies extends Component {
     state = {
         movies : getMovies(),
         pageSize : 5,
-        currentPage : 1
+        currentPage : 1,
+        genre : getGenres(),
     };
 
     handleDelete = (movie) => {
@@ -28,6 +32,10 @@ class Movies extends Component {
     handlePageChange = (page) => {
         this.setState({currentPage : page});
     };
+
+    handleGenreSelect = (item) => {
+        console.log(item);
+    }
     render() {
 
         const { length : count } = this.state.movies;
@@ -39,43 +47,51 @@ class Movies extends Component {
         const movies = paginate(this.state.movies, currentPage, pageSize); //调用分页算法
 
         return (
-            <React.Fragment>
-                <p>数据库中有{count}部电影</p>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Genre</th>
-                            <th>Stock</th>
-                            <th>Rate</th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {movies.map(movie => (
-                            <tr key={movie._id}>
-                                <td>{movie.title}</td>
-                                <td>{movie.genre.name}</td>
-                                <td>{movie.numberInStock}</td>
-                                <td>{movie.dailyRentalRate}</td>
-                                <td>
-                                    <Like liked = {movie.liked} onClick={() => this.handleLike(movie)} />
-                                </td>
-                                <td>
-                                    <button onClick={() => this.handleDelete(movie)} className='btn btn-danger btn-sm'>删除</button>
-                                </td>
+            <div className='row'>
+                <div className='col-2'>
+                    <ListGroup
+                        items={this.state.genre}
+                        onItemSelect = {this.handleGenreSelect}
+                    />
+                </div>
+                <div className='col-5'>
+                    <p>数据库中有{count}部电影</p>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Genre</th>
+                                <th>Stock</th>
+                                <th>Rate</th>
+                                <th></th>
+                                <th></th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
-                <Pagination
-                    itemsCount={count}
-                    pageSize={pageSize}
-                    currentPage={currentPage}
-                    onPageChange={this.handlePageChange}
-                />
-            </React.Fragment>
+                        </thead>
+                        <tbody>
+                            {movies.map(movie => (
+                                <tr key={movie._id}>
+                                    <td>{movie.title}</td>
+                                    <td>{movie.genre.name}</td>
+                                    <td>{movie.numberInStock}</td>
+                                    <td>{movie.dailyRentalRate}</td>
+                                    <td>
+                                        <Like liked = {movie.liked} onClick={() => this.handleLike(movie)} />
+                                    </td>
+                                    <td>
+                                        <button onClick={() => this.handleDelete(movie)} className='btn btn-danger btn-sm'>删除</button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                    <Pagination
+                        itemsCount={count}
+                        pageSize={pageSize}
+                        currentPage={currentPage}
+                        onPageChange={this.handlePageChange}
+                    />
+                </div>
+            </div>
         )}
 }
 
