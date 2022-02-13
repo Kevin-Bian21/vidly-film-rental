8bc6f5ee11fd79f658346922e49bdda65580eec8
BianWenKai <1467295546@qq.com>
2022-02-13 16:26:13 +0800

    添加导航栏

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 import Movies from './components/movies';
+import Customers from './components/customers';
+import NotFound from './components/notFound';
+import Rentals from './components/rentals';
+import NavBar from './components/navBar';
+import { Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <main className='container'>
-      <Movies/>
-    </main>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <main className='container'>
+          <Routes>
+            <Route path="movies" element={<Movies/>}></Route>
+            <Route path="customers" element={<Customers/>}></Route>
+            <Route path="rentals" element={<Rentals/>}></Route>
+            <Route path="*" element={<NotFound/>}></Route>
+          </Routes>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
