71a9801de53bdfa4aa78a034947904004a6051a2
BianWenKai <1467295546@qq.com>
2022-02-15 00:09:13 +0800

    修复表单无法动态校验的问题

--- vidly/src/components/common/Login.jsx
+++ vidly/src/components/common/Login.jsx
@@ -6,28 +6,16 @@ class Login extends Component {
 
     state= {
         account : {
-            username : '',
-            password : ''
+            username : "",
+            password : ""
         },
         errors : { }
-    }
-
-    handleSubmit = e => {
-        e.preventDefault();
-
-        const errors = this.validate();
-        console.log(errors);
-        this.setState({ errors : errors || {} });
-
-        if (errors)
-            return;
-        console.log("submitted");
-    }
+    };
 
     schema = {
         username : Joi.string().required().label('Usename'),
         password : Joi.string().required().label('Password')
-    }
+    };
 
     validate = () => {
         const options = { abortEarly : false }; //遇到错误不用提前终止
@@ -39,23 +27,42 @@ class Login extends Component {
         for (let item of result.error.details)
             errors[item.path[0]] = item.message;
         return errors;
-    }
+    };
 
     validateProperty = ({ name, value }) => {
         const obj = { [name] : value };
         const schema = { [name] : this.schema[name]};
-        Joi.validate(obj, schema );
         const { error } = Joi.validate(obj, schema);
-        return error ? null : error.details[0].message;
-    }
+        return error ? error.details[0].message : null;
+    };
 
     //当输入框中的内容发生改变时就会触发该方法
     handleChange = e => {
+        //动态的校验输入框的改变
+        const errors = {...this.state.errors};
+        const errorMessage = this.validateProperty(e.currentTarget);
+        if (errorMessage)
+            errors[e.currentTarget.name] = errorMessage;
+        else
+            delete errors[e.currentTarget.name];
+
         console.log(e)
         const account = {...this.state.account};
         account[e.currentTarget.name] = e.currentTarget.value;  // 通过input框中的 name 属性获取当前具体输入的标签然后将其赋值给state
-        this.setState({account })
-    }
+        this.setState({account, errors })
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        const errors = this.validate();
+        console.log(errors);
+        this.setState({ errors : errors || {} });
+
+        if (errors)
+            return;
+        console.log("submitted");
+    };
 
     render() {
         return (
@@ -65,7 +72,7 @@ class Login extends Component {
                     <div className="mb-3">
                         <label htmlFor="username">Username</label>
                         <input value={this.state.account.username} onChange={this.handleChange} name='username' autoFocus id='username' type="text" className="form-control" />
-                        {  this.state.errors.username && <div className='alert alert-danger'>{this.state.errors.username}</div> }
+                         {  this.state.errors.username && <div className='alert alert-danger'>{this.state.errors.username}</div> }
                     </div>
                     <div className="mb-3">
                         <label htmlFor="password">Password</label>
--- vidly/src/components/common/Register.jsx
+++ vidly/src/components/common/Register.jsx
@@ -11,26 +11,14 @@ class Register extends Component {
         errors : {}
      }
 
-    handleSubmit = e => {
-        e.preventDefault();
+     schema = {
+         username : Joi.string().required().email().label('Usename'),
+         password : Joi.string().required().min(5).label('Password'),
+         name : Joi.string().required().label('Name')
+        };
 
-        const errors = this.validate();
-        console.log(errors);
-        this.setState({ errors : errors || {} });
-
-        if (errors)
-            return;
-        console.log("submitted");
-    }
-
-    schema = {
-        username : Joi.string().required().email().label('Usename'),
-        password : Joi.string().required().min(5).label('Password'),
-        name : Joi.string().required().label('Name')
-    };
-
-    validate = () => {
-        const options = { abortEarly : false }; //遇到错误不用提前终止
+        validate = () => {
+            const options = { abortEarly : false }; //遇到错误不用提前终止
         const result = Joi.validate(this.state.data, this.schema, options);
         if (!result.error)
             return null;
@@ -44,18 +32,38 @@ class Register extends Component {
     validateProperty = ({ name, value }) => {
         const obj = { [name] : value };
         const schema = { [name] : this.schema[name]};
-        Joi.validate(obj, schema );
         const { error } = Joi.validate(obj, schema);
-        return error ? null : error.details[0].message;
+        return error ? error.details[0].message : null;
     }
 
     //当输入框中的内容发生改变时就会触发该方法
     handleChange = e => {
+        //动态的校验输入框的改变
+        const errors = {...this.state.errors};
+        const errorMessage = this.validateProperty(e.currentTarget);
+        if (errorMessage)
+        errors[e.currentTarget.name] = errorMessage;
+        else
+            delete errors[e.currentTarget.name];
+
         console.log(e)
         const data = {...this.state.data};
         data[e.currentTarget.name] = e.currentTarget.value;  // 通过input框中的 name 属性获取当前具体输入的标签然后将其赋值给state
-        this.setState({ data })
+        this.setState({data, errors })
     }
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        const errors = this.validate();
+        console.log(errors);
+        this.setState({ errors : errors || {} });
+
+        if (errors)
+            return;
+        console.log("submitted");
+    }
+
     render() {
         return (
             <div>
