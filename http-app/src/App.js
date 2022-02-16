e5e8edf1d72f5c6a543335c26a7ccedc84191e17
BianWenKai <1467295546@qq.com>
2022-02-16 19:28:03 +0800

    封装配置文件config.json

--- http-app/src/App.js
+++ http-app/src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 import http from './services/httpService';
+import config from './config.json';
 import "./App.css";
-
-
-const apiEndpoint = 'http://jsonplaceholder.typicode.com/posts';
 
 class App extends Component {
   state = {
@@ -12,20 +10,20 @@ class App extends Component {
 
   async componentDidMount() {
     // pending > resolved (success) OR rejected (failure)
-    const { data: posts } = await http.get(apiEndpoint);
+    const { data: posts } = await http.get(config.apiEndpoint);
     this.setState({ posts });
   }
 
   handleAdd = async () => {
     const obj = {title : 'hello world', body :'thank you'};
-    const { data : post } = await http.post(apiEndpoint, obj);
+    const { data : post } = await http.post(config.apiEndpoint, obj);
     const posts = [post, ...this.state.posts];
     this.setState({posts});
   };
 
   handleUpdate = async post => {
     post.title = "UPDATED";
-    await http.put(apiEndpoint + "/" + post.id, post);
+    await http.put(config.apiEndpoint + "/" + post.id, post);
 
     const posts = [...this.state.posts];
     const index = posts.indexOf(post);
@@ -41,7 +39,7 @@ class App extends Component {
     this.setState({ posts });
 
     try {
-      http.delete(apiEndpoint + '/' + post.id);
+      http.delete(config.apiEndpoint + '/' + post.id);
     } catch (e) {
       if (e.response && e.response.status === 404)
         alert('This post has already benn deleted');
