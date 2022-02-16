4e5175a281bb38061dbbb2959b370fe1d72d8750
BianWenKai <1467295546@qq.com>
2022-02-16 20:44:41 +0800

    通过sentry.io网站部署日志出错记录

--- http-app/src/services/httpService.js
+++ http-app/src/services/httpService.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 import { toast } from 'react-toastify';
+import * as Sentry from "@sentry/react";
 
 //使用axios拦截器处理未知的异常并将异常记录在日志中
 axios.interceptors.response.use(null, error => {
     const expectedError = error.response && error.response.status >= 400 && error.response.status < 500;
 
     if (! expectedError) {
-      console.log('Loging the error', error);
-      toast.error("发生未知错误");
+        Sentry.captureException(error);
+        toast.error("发生未知错误");
     }
       return Promise.reject(error);
   });
